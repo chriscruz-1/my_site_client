7d380f481216e8564c9f4d9dd7748a42338fd278
Chris Cruz <41966126+chriscruz-1@users.noreply.github.com>
2021-09-24 20:56:56 -0700

    #1 - Installed reactstrap and bootstrap. Imported bootstrap css. Created template for Navbar.

---
+++ my_site/src/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import MyNarbar from './MyNavbar'
+
+const Home = () => {
+  return (
+    <div className="Home">
+      <MyNarbar />
+    </div>
+  );
+}
+
+export default Home;
