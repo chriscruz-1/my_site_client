0c1a41c20df8cc0d44e5dc2c80634f7cd1c07d4d
Chris Cruz <41966126+chriscruz-1@users.noreply.github.com>
2021-09-25 16:59:01 -0700

    #1 - Placed all Navbar content into a container and row. Used columns to appropriately space out my Navbar as desired. Used text align center and justify content center to center each column's children

--- my_site/src/MyNavbar.jsx
+++ my_site/src/MyNavbar.jsx
@@ -1,11 +1,35 @@
 import React, { useState } from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 const MyNavbar = () => {
-  const [isOpen, setIsOpen] = useState(false);
+  // const [isOpen, setIsOpen] = useState(false);
   return (
-    <div>
-      
-    </div>
+    <Container fluid>
+      <Row>
+      <Navbar bg="dark" variant="dark" style={{textAlign: "center"}}>
+        <Col>
+          <Container>
+            <Nav className="justify-content-center">
+              <Nav.Link href="#about">About</Nav.Link>
+              <NavDropdown title="Projects" id="navbarScrollingDropdown">
+                <NavDropdown.Item href="#1rmc">1 Rep Max Calculator</NavDropdown.Item>
+                <NavDropdown.Item href="#fitness_tracker">Fitness Tracker</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Container>
+        </Col>
+        <Col md={2}>
+          <Navbar.Brand href="#home">Chris Cruz</Navbar.Brand>
+        </Col>
+        <Col>Contacts</Col>
+        </Navbar>
+      </Row>
+    </Container>
   );
 }
 
