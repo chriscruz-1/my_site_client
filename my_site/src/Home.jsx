773e244ffe9e7346019eb648ecbfd790a82900cf
Chris Cruz <41966126+chriscruz-1@users.noreply.github.com>
2021-09-26 13:49:42 -0700

    #2 - Added image for homepage

--- my_site/src/Home.jsx
+++ my_site/src/Home.jsx
@@ -2,12 +2,23 @@ import React from 'react';
 import './Home.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import MyNarbar from './MyNavbar'
+import Image from 'react-bootstrap/Image';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import { Container } from 'react-bootstrap';
 
 const Home = () => {
   return (
-    <div className="Home">
+    <>
       <MyNarbar />
-    </div>
+      <Container>
+        <Row className="justify-content-md-center">
+          <Col md="auto">
+            <Image src="prof_shoot.png" roundedCircle style={{width: "70%", height: "auto", display: "block", margin: "auto"}}/>
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 }
 
