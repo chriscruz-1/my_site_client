022e7b05e8f1f4b69a0561c03a34a92770b98803
Chris Cruz <41966126+chriscruz-1@users.noreply.github.com>
2021-09-27 07:47:38 -0700

    #2 - Switched from Image component to Figure component

--- my_site/src/Home.jsx
+++ my_site/src/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './Home.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import MyNarbar from './MyNavbar'
-import Image from 'react-bootstrap/Image';
+import Figure from 'react-bootstrap/Figure'
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import { Container } from 'react-bootstrap';
@@ -14,7 +14,20 @@ const Home = () => {
       <Container>
         <Row className="justify-content-md-center">
           <Col md="auto">
-            <Image src="prof_shoot.png" roundedCircle style={{width: "70%", height: "auto", display: "block", margin: "auto"}}/>
+          {/* For getting image to center inside a Bootstrap column: 
+          https://stackoverflow.com/questions/44055130/how-to-center-image-within-bootstrap-column */}
+            <Figure>
+              <Figure.Image
+                width={"70%"}
+                height={"auto"}
+                alt="MyProfessionalPhoto"
+                src="prof_shoot.png"
+                style={{display: "block", margin: "auto", borderRadius: "70%"}}
+              />
+              <Figure.Caption style={{textAlign: "center"}}>
+                Hello, I'm Chris, a full stack web developer. Welcome to my site!
+              </Figure.Caption>
+            </Figure>
           </Col>
         </Row>
       </Container>
