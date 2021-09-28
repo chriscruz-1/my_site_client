97c73ad07b0ea61e890302f7041eba3b05a589cc
Chris Cruz <41966126+chriscruz-1@users.noreply.github.com>
2021-09-27 22:33:01 -0700

    #2 - Added mountain image for background

--- my_site/src/Home.jsx
+++ my_site/src/Home.jsx
@@ -1,36 +1,15 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import MyNarbar from './myNavbar'
-import Figure from 'react-bootstrap/Figure'
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
-import { Container } from 'react-bootstrap';
+import Image from 'react-bootstrap/Image';
 
 const Home = () => {
   return (
     <>
       <MyNarbar />
-      <Container>
-        <Row className="justify-content-md-center">
-          <Col md="auto">
-          {/* For getting image to center inside a Bootstrap column: 
-          https://stackoverflow.com/questions/44055130/how-to-center-image-within-bootstrap-column */}
-            <Figure>
-              <Figure.Image
-                width={"70%"}
-                height={"auto"}
-                alt="MyProfessionalPhoto"
-                src="prof_shoot.png"
-                style={{display: "block", margin: "auto", borderRadius: "70%"}}
-              />
-              <br/>
-              <Figure.Caption style={{textAlign: "center"}}>
-                Hello, I'm Chris, a full stack web developer. Welcome to my site!
-              </Figure.Caption>
-            </Figure>
-          </Col>
-        </Row>
-      </Container>
+      <Image fluid
+        src="explorer.png"
+      />
     </>
   );
 }
