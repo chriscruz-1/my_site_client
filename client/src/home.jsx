7cdee4452f1ea1577078264a37f53644deacd741
Chris Cruz <41966126+chriscruz-1@users.noreply.github.com>
2021-10-03 12:30:32 -0700

    #5 - Added default style props for the p tag so they also can be overwritten

--- client/src/home.jsx
+++ client/src/home.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Image from 'react-bootstrap/Image';
 
-const Home = () => {
+const Home = ({bigPProp, smallPProp}) => {
 
   //Reference for getting window size https://stackoverflow.com/questions/44480053/how-to-detect-if-screen-size-has-changed-to-mobile-in-react
   const getWindowDimensions = () => {
@@ -27,13 +27,14 @@ const Home = () => {
 
     return windowDimensions;
   }
-
+  let bigPStyle = bigPProp === undefined ? { fontSize: "2em",display: "inline", position: "relative", top: "10em", left: "10em", zIndex: "2"} : bigPProp;
+  let smallPStype = smallPProp === undefined ? { display: "inline", position: "relative", top: "10em", left: "10em", zIndex: "2"} : smallPProp;
   return (
     <>
       {(useWindowDimensions().width > 1100) ? 
-        <p style={{ fontSize: "2em",display: "inline", position: "relative", top: "10em", left: "10em", zIndex: "2"}}>Web developer. Explorer. Learner.</p>
+        <p style={bigPStyle}>Web developer. Explorer. Learner.</p>
         :
-        <p style={{ display: "inline", position: "relative", top: "10em", left: "10em", zIndex: "2"}}>Web developer. Explorer. Learner.</p>
+        <p style={smallPStype}>Web developer. Explorer. Learner.</p>
       }
       <Image
         src="explorer.png"
