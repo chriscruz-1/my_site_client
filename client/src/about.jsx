137816c3defbebf54d1389c8c9ff6fe41a68fdb3
Chris Cruz <41966126+chriscruz-1@users.noreply.github.com>
2021-10-03 12:30:56 -0700

    Added container so image and text are in their own responsive rows.

--- client/src/about.jsx
+++ client/src/about.jsx
@@ -1,15 +1,31 @@
 import React from 'react';
 import Image from 'react-bootstrap/Image';
 import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
-const About = () => {
+const About = ({pStyleProp}) => {
+  let pStyle = pStyleProp === undefined ? {className: "justify-content-center", marginTop: "3em"} : pStyleProp;
   return(
     <Container fluid>
-      <Image 
-        src="prof_shoot.png"
-        width="50%"
-        roundedCircle
-      />
+      <Row>
+        <Col>
+          <Image 
+            src="prof_shoot.png"
+            width="100%"
+            roundedCircle
+          />
+        </Col>
+        <Col>
+          <p style={pStyle}>
+            I'm a full stack web developer. Recently graduated from University of California, Davis with a Bachelors of Science in Computer Science.
+          </p>
+          <p style={pStyle}>
+            About 1.5 years of experience
+          </p>
+
+        </Col>
+      </Row>
     </Container>
   );
 }
