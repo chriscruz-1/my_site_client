fd5a138a8499864ba124f5a930789cd9da29fd35
Chris Cruz <41966126+chriscruz-1@users.noreply.github.com>
2021-10-03 19:38:25 -0700

    #5 - Put content into a container so content is dynamically spaced

--- client/src/about.jsx
+++ client/src/about.jsx
@@ -7,26 +7,27 @@ import Col from 'react-bootstrap/Col';
 const About = ({pStyleProp}) => {
   let pStyle = pStyleProp === undefined ? {className: "justify-content-center", marginTop: "3em"} : pStyleProp;
   return(
-    <Container fluid>
-      <Row>
-        <Col>
-          <Image 
-            src="prof_shoot.png"
-            width="100%"
-            roundedCircle
-          />
-        </Col>
-        <Col>
-          <p style={pStyle}>
-            I'm a full stack web developer. Recently graduated from University of California, Davis with a Bachelors of Science in Computer Science.
-          </p>
-          <p style={pStyle}>
-            About 1.5 years of experience
-          </p>
-
-        </Col>
-      </Row>
-    </Container>
+    <>
+      <Container fluid>
+        <Row>
+          <Col>
+            <Image 
+              src="prof_shoot.png"
+              width="100%"
+              roundedCircle
+            />
+          </Col>
+          <Col>
+            <p style={pStyle}>
+              Full stack web developer. Recently graduated from University of California, Davis with a Bachelors of Science in Computer Science.
+            </p>
+            <p style={pStyle}>
+              About 1.5 years of experience
+            </p>
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 }
 
