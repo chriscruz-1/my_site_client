c5c5cf6f2b89ec0eeb35af68d8b431b1b920333b
Chris Cruz <41966126+chriscruz-1@users.noreply.github.com>
2021-10-03 21:06:03 -0700

    #5 - Removed window dimensions finder to become its own component

--- client/src/home.jsx
+++ client/src/home.jsx
@@ -1,32 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Image from 'react-bootstrap/Image';
+import useWindowDimensions from './use_window_dimensions';
 
 const Home = ({bigPProp, smallPProp}) => {
 
-  //Reference for getting window size https://stackoverflow.com/questions/44480053/how-to-detect-if-screen-size-has-changed-to-mobile-in-react
-  const getWindowDimensions = () => {
-    const { innerWidth: width, innerHeight: height } = window;
-    return {
-      width,
-      height
-    };
-  };
-
-  const useWindowDimensions = () => {
-    let [ windowDimensions, setWindowDimensions ] = useState(getWindowDimensions());
-
-    useEffect(() => {
-      const handleResize = () => {
-        setWindowDimensions(getWindowDimensions());
-      };
-  
-      window.addEventListener('resize', handleResize);
-      return () => window.removeEventListener('resize', handleResize);
-    }, []);
-
-    return windowDimensions;
-  }
   let bigPStyle = bigPProp === undefined ? { fontSize: "2em",display: "inline", position: "relative", top: "10em", left: "10em", zIndex: "2"} : bigPProp;
   let smallPStype = smallPProp === undefined ? { display: "inline", position: "relative", top: "10em", left: "10em", zIndex: "2"} : smallPProp;
   return (
