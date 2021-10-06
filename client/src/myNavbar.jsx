3c5875ccd4447b2ea3d3e761d92352e969b33d63
Chris Cruz <41966126+chriscruz-1@users.noreply.github.com>
2021-10-05 20:20:36 -0700

    #13 - Added Navbar.collapse forr Hamburger menu when viewport is small

--- client/src/myNavbar.jsx
+++ client/src/myNavbar.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
@@ -17,52 +16,53 @@ import {
 const MyNavbar = () => {
   return (
     <Router>
-      <Container fluid>
-        <Row>
-        <Navbar bg="light" variant="light" style={{textAlign: "center"}}>
-          <Col>
-            <Container>
+      <Navbar bg="light" expand="md" variant="light" style={{textAlign: "center"}}>
+        <Container>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav"> 
+            <Col>
+              <Container>
+                <Nav className="justify-content-center">
+                  <Nav.Link href="/">Home</Nav.Link>
+                  <Nav.Link href="/about">About</Nav.Link>
+                  <NavDropdown title="Projects" id="navbarScrollingDropdown">
+                    <NavDropdown.Item href="/orm">1 Rep Max Calculator (In progress)</NavDropdown.Item>
+                    <NavDropdown.Item href="/fitnesstracker">Fitness Tracker (In progress)</NavDropdown.Item>
+                  </NavDropdown>
+                </Nav>
+              </Container>
+            </Col>
+            <Col md={2}>
+              <Navbar.Brand href="#home">Chris Cruz</Navbar.Brand>
+            </Col>
+            <Col>
               <Nav className="justify-content-center">
-                <Nav.Link href="/">Home</Nav.Link>
-                <Nav.Link href="/about">About</Nav.Link>
-                <NavDropdown title="Projects" id="navbarScrollingDropdown">
-                  <NavDropdown.Item href="/orm">1 Rep Max Calculator (In progress)</NavDropdown.Item>
-                  <NavDropdown.Item href="/fitnesstracker">Fitness Tracker (In progress)</NavDropdown.Item>
-                </NavDropdown>
+                <Nav.Item>
+                  <Nav.Link href="mailto:chriscruz.1@icloud.com">
+                    <i className="far fa-envelope fa-lg"></i>
+                  </Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link href="http://www.linkedin.com/in/chriscruz-1">
+                    <i className="fab fa-linkedin fa-lg"></i>
+                  </Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link href="http://www.github.com/chriscruz-1">
+                    <i className="fab fa-github fa-lg"></i>
+                  </Nav.Link>
+                </Nav.Item>
               </Nav>
-            </Container>
-          </Col>
-          <Col md={2}>
-            <Navbar.Brand href="#home">Chris Cruz</Navbar.Brand>
-          </Col>
-          <Col>
-            <Nav className="justify-content-center">
-              <Nav.Item>
-                <Nav.Link href="mailto:chriscruz.1@icloud.com">
-                  <i className="far fa-envelope fa-lg"></i>
-                </Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link href="http://www.linkedin.com/in/chriscruz-1">
-                  <i className="fab fa-linkedin fa-lg"></i>
-                </Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link href="http://www.github.com/chriscruz-1">
-                  <i className="fab fa-github fa-lg"></i>
-                </Nav.Link>
-              </Nav.Item>
-            </Nav>
-          </Col>
-          </Navbar>
-        </Row>
+            </Col>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
 
         <Switch>
           <Route exact path="/" component={Home}/>
           <Route path="/about" component={About}/>
           <Redirect to="/"/>
         </Switch>
-      </Container>
     </Router>
   );
 }
