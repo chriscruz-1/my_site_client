66c128780f8aaf34c642be44a0a2688a30fc9ce9
Chris Cruz <41966126+chriscruz-1@users.noreply.github.com>
2021-10-13 21:36:34 -0700

    #6 - Created function for calculating PR. Added handlers for exercise and reps. Added headers for displaying ORM

--- client/src/orm.jsx
+++ client/src/orm.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
@@ -13,6 +13,8 @@ const Orm = ({
   //Hooks
   let [ username, setUsername ] = useState(null);
   let [ exercise, setExercise ] = useState(null);
+  let [ weight, setWeight ] = useState(null);
+  let [ numReps, setNumReps ] = useState(null);
 
   // Need to use a handler for onChange. https://stackoverflow.com/questions/63484886/get-value-of-select-onchange-in-functional-component
   //Handlers:
@@ -24,6 +26,15 @@ const Orm = ({
     setExercise(event.target.value);
   };
 
+  const weightSelectHandler = (event) => {
+    setWeight(event.target.value);
+  };
+
+  const repSelectHandler = (event) => {
+    setNumReps(event.target.value);
+  };
+
+  //For dynamically providing select options
   const getWeightRange = () => {
     let weights = [];
     for(let i = 0; i < maxWeight; i+= 20){
@@ -38,6 +49,14 @@ const Orm = ({
       reps.push(i);
     }
     return reps;
+  };
+
+  const getPR = () => {
+    let estORM = null;
+    if(weight !== null && numReps !== null){
+      estORM = (weight * (36 / (37 - numReps))).toFixed(2);
+    }
+    return estORM;
   };
 
   return(
@@ -56,7 +75,7 @@ const Orm = ({
           <Row>
             <Col>
                 <Form.Group>
-                  <Form.FloatingLabel controlId="floatingSelect" label="Exercise">
+                  <Form.FloatingLabel controlId="floatingSelect" label="exercise">
                     <Form.Select onChange={exerciseSelectHandler}>
                       <option>Select an exercise</option>
                       <option value="bench">Bench</option>
@@ -69,8 +88,8 @@ const Orm = ({
             </Col>
             <Col>
               <Form.Group>
-                <Form.FloatingLabel controlId="floatingSelect" label="Weight">
-                  <Form.Select id="weightsSelect">
+                <Form.FloatingLabel controlId="floatingSelect" label="weight">
+                  <Form.Select id="weightsSelect" onChange={weightSelectHandler}>
                     <option>Select the weight for your set</option>
                     {getWeightRange().map((d,i) => {
                       return(<option>{d}</option>);
@@ -79,8 +98,28 @@ const Orm = ({
                 </Form.FloatingLabel>
               </Form.Group>
             </Col>
+            <Col>
+              <Form.Group>
+                <Form.FloatingLabel controlId="floatingSelect" label="reps">
+                  <Form.Select id="repsSelect" onChange={repSelectHandler}>
+                    <option>Select the number of reps for your set</option>
+                    {getRepRange().map((d,i) => {
+                      return(<option>{d+1}</option>)
+                    })}
+                  </Form.Select>
+                </Form.FloatingLabel>
+              </Form.Group>
+            </Col>
           </Row>
         </Form>
+        <Row>
+          {username && exercise && weight && numReps &&
+            <>
+              <h2>Your estimated one rep max for {exercise} is {getPR()} lbs! Great job {username}!</h2>
+              <h4>Your one rep max was calculated with <a href="https://www.athlegan.com/calculate-1rm">Brzycki's formula</a></h4>
+            </>
+          }
+        </Row>
       </Container>
     </>
   );  
