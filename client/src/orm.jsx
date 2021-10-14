0848b39dbba166d2024083a5cbd3d19f6e182b1a
Chris Cruz <41966126+chriscruz-1@users.noreply.github.com>
2021-10-13 20:36:15 -0700

    #6 - Added hook to save exercise chosen by user

--- client/src/orm.jsx
+++ client/src/orm.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Form from 'react-bootstrap/Form';
 
 const Orm = () => {
+  let [ exercise, setExercise ] = useState(null);
+
+  // Need to use a handler for onChange. https://stackoverflow.com/questions/63484886/get-value-of-select-onchange-in-functional-component
+  const exerciseSelectHandler = (event) => {
+    setExercise(event.target.value);
+  };
+
   return(
     <>
-      <Form.FloatingLabel controlId="floatingSelect" label="Exercise">
-        <Form.Select id="exerciseSelect">
+      <Form.FloatingLabel id="testSelect" controlId="floatingSelect" label="Exercise">
+        <Form.Select id="exerciseSelect" onChange={exerciseSelectHandler}>
           <option>Select an exercise</option>
           <option value="bench">Bench</option>
           <option value="squat">Squat</option>
