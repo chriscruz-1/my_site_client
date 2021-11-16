0c8672712b53f05fb7a8cc8ec6e1fbeb62a01296
Chris Cruz <41966126+chriscruz-1@users.noreply.github.com>
2021-11-15 19:27:20 -0800

    Fixed whitespace bug on homepage by changing position to absolute

--- src/home.jsx
+++ src/home.jsx
@@ -5,8 +5,8 @@ import useWindowDimensions from './use_window_dimensions';
 
 const Home = ({bigPProp, smallPProp}) => {
 
-  let bigPStyle = bigPProp === undefined ? { fontSize: "2em",display: "inline", position: "relative", top: "10em", left: "10em", zIndex: "2"} : bigPProp;
-  let smallPStype = smallPProp === undefined ? { fontSize: "0.5em", display: "inline", position: "relative", top: "10em", left: "10em", zIndex: "2"} : smallPProp;
+  let bigPStyle = bigPProp === undefined ? { fontSize: "2em",display: "inline", position: "absolute", top: "10em", left: "10em", zIndex: "2"} : bigPProp;
+  let smallPStype = smallPProp === undefined ? { fontSize: "0.5em", display: "inline", position: "absolute", top: "10em", left: "10em", zIndex: "2"} : smallPProp;
   return (
     <>
       {(useWindowDimensions().width > 1100) ? 
