0d9816cc146d369cbad185c135d115a1f375210f
Chris Cruz <41966126+chriscruz-1@users.noreply.github.com>
2021-11-22 12:13:47 -0800

    #27 - Moved homepage caption to below image when viewport smaller than 1100

--- src/home.jsx
+++ src/home.jsx
@@ -5,19 +5,30 @@ import useWindowDimensions from './use_window_dimensions';
 
 const Home = ({bigPProp, smallPProp}) => {
 
-  let bigPStyle = bigPProp === undefined ? { fontSize: "2em",display: "inline", position: "absolute", top: "10em", left: "10em", zIndex: "2"} : bigPProp;
-  let smallPStype = smallPProp === undefined ? { fontSize: "0.5em", display: "inline", position: "absolute", top: "10em", left: "10em", zIndex: "2"} : smallPProp;
+  let bigPStyle = bigPProp === undefined ? { 
+    fontSize: "2em",display: "inline", position: "absolute", top: "10em", left: "10em", zIndex: "2"
+  } 
+  : 
+  bigPProp;
+
+  let smallPStype = smallPProp === undefined ? { 
+    fontSize: "1.5em", textAlign: "center", position: "relative"
+  } 
+  : 
+  smallPProp;
+
   return (
     <>
-      {(useWindowDimensions().width > 1100) ? 
+      {(useWindowDimensions().width > 1100) && 
         <p style={bigPStyle}>Web developer. Explorer. Learner.</p>
-        :
-        <p style={smallPStype}>Web developer. Explorer. Learner.</p>
       }
       <Image
         src="explorer.png"
         width="100%"
       />
+      {(useWindowDimensions().width <= 1100) && 
+        <p style={smallPStype}>Web developer. Explorer. Learner.</p>
+      }
     </>
   );
 }
