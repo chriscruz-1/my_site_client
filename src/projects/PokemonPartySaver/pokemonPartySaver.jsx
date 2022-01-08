857c68161bbc3016fe3b2e4cc0ff500d57d79aaa
Chris Cruz <41966126+chriscruz-1@users.noreply.github.com>
2022-01-08 12:15:51 -0800

    #35 - Created fetch request that returns data from Pokemon API

--- src/projects/PokemonPartySaver/pokemonPartySaver.jsx
+++ src/projects/PokemonPartySaver/pokemonPartySaver.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row'
@@ -11,6 +11,7 @@ const PokemonPartySaver = ({
 }) => {
   let [lookup, setLookup] = useState(null);
   let [searchBy, setSearchBy] = useState(null);
+  let [pokemonName, setPokemonName] = useState(null);
 
   const lookupHandler = (event) => {
     setLookup(event.target.value);
@@ -18,14 +19,36 @@ const PokemonPartySaver = ({
 
   const searchByHandler = (event) => {
     setSearchBy(event.target.value);
+  };
+
+  const pokemonNameHandler = (event) => {
+    setPokemonName(event.target.value)
   }
+
+  const fetchData = async (url = '', lookup = '', name = '' ) => {
+    const response = await fetch(`${url}/${lookup}/${name}`);
+    return response.json();
+  };
+
+  const returnResults = async () => {
+    let results = await fetchData('https://pokeapi.co/api/v2', `${lookup}`, pokemonName);
+    return results;
+  };
+
+  const submitHandler = async (event) => {
+    event.preventDefault();
+    returnResults()
+      .then(data => {
+        console.log(data);
+      })
+  };
 
   return(
     <>
       <h1 style={appTitleStyle}>Welcome to the Pokemon Party Saver!</h1>
       <h2 style={appTitleStyle}>Pokedex Lookup</h2>
       <Container>
-        <Form>
+        <Form onSubmit={submitHandler}>
           <Form.Group>
             <Form.Label>Lookup: </Form.Label>
             <Form.Select size="md" style={inputStyle} onChange={lookupHandler}>
@@ -45,7 +68,7 @@ const PokemonPartySaver = ({
             {searchBy === "name" && 
               <>
                 <Form.Label>Name: </Form.Label>
-                <Form.Control style={inputStyle} placeholder="Enter Pokemon's Name" />
+                <Form.Control id='pokemonName' style={inputStyle} placeholder="Enter Pokemon's Name" onChange={pokemonNameHandler}/>
                 <Button variant="primary" type="submit" size='md'>
                   Submit
                 </Button>
