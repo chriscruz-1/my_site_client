905e3fa660e25cfd628782fb8be6f69e7fa2df6d
Chris Cruz <41966126+chriscruz-1@users.noreply.github.com>
2022-01-08 14:02:43 -0800

    #35 - Rendering Pokemon image when user clicks submit

--- src/projects/PokemonPartySaver/pokemonPartySaver.jsx
+++ src/projects/PokemonPartySaver/pokemonPartySaver.jsx
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row'
+import Row from 'react-bootstrap/Row';
 import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button'
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 const PokemonPartySaver = ({
   appTitleStyle = {textAlign: "center"},
@@ -12,6 +13,11 @@ const PokemonPartySaver = ({
   let [lookup, setLookup] = useState(null);
   let [searchBy, setSearchBy] = useState(null);
   let [pokemonName, setPokemonName] = useState(null);
+  let [data, setData] = useState(null);
+
+  useEffect(() => {
+    console.log(data)
+  }, [data]);
 
   const lookupHandler = (event) => {
     setLookup(event.target.value);
@@ -26,7 +32,7 @@ const PokemonPartySaver = ({
   }
 
   const fetchData = async (url = '', lookup = '', name = '' ) => {
-    const response = await fetch(`${url}/${lookup}/${name}`);
+    const response = await fetch(`${url}/${lookup}/${name.toLowerCase()}`);
     return response.json();
   };
 
@@ -37,10 +43,12 @@ const PokemonPartySaver = ({
 
   const submitHandler = async (event) => {
     event.preventDefault();
-    returnResults()
-      .then(data => {
-        console.log(data);
-      })
+    try {
+      const results = await returnResults();
+      setData(results);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return(
@@ -48,34 +56,47 @@ const PokemonPartySaver = ({
       <h1 style={appTitleStyle}>Welcome to the Pokemon Party Saver!</h1>
       <h2 style={appTitleStyle}>Pokedex Lookup</h2>
       <Container>
-        <Form onSubmit={submitHandler}>
-          <Form.Group>
-            <Form.Label>Lookup: </Form.Label>
-            <Form.Select size="md" style={inputStyle} onChange={lookupHandler}>
-              <option value="null">Select...</option>
-              <option value="pokemon">Pokemon</option>
-            </Form.Select>
-            {lookup === "pokemon" && 
+        <Row>
+          {data !== null && 
+            <Col>
               <>
-                <Form.Label>Search by: </Form.Label>
-                <Form.Select size="md" style={inputStyle} onChange={searchByHandler}>
+                <Card>
+                  <Card.Img src={data.sprites.front_default}></Card.Img>
+                </Card>
+              </>
+            </Col>
+          }
+          <Col>
+            <Form onSubmit={submitHandler}>
+              <Form.Group>
+                <Form.Label>Lookup: </Form.Label>
+                <Form.Select size="md" style={inputStyle} onChange={lookupHandler}>
                   <option value="null">Select...</option>
-                  <option value="name">Name</option>
-                  <option value="number">Number</option>
+                  <option value="pokemon">Pokemon</option>
                 </Form.Select>
-              </>
-            }
-            {searchBy === "name" && 
-              <>
-                <Form.Label>Name: </Form.Label>
-                <Form.Control id='pokemonName' style={inputStyle} placeholder="Enter Pokemon's Name" onChange={pokemonNameHandler}/>
-                <Button variant="primary" type="submit" size='md'>
-                  Submit
-                </Button>
-              </>
-            }
-          </Form.Group>
-        </Form>
+                {lookup === "pokemon" && 
+                  <>
+                    <Form.Label>Search by: </Form.Label>
+                    <Form.Select size="md" style={inputStyle} onChange={searchByHandler}>
+                      <option value="null">Select...</option>
+                      <option value="name">Name</option>
+                      <option value="number">Number</option>
+                    </Form.Select>
+                  </>
+                }
+                {searchBy === "name" && 
+                  <>
+                    <Form.Label>Name: </Form.Label>
+                    <Form.Control id='pokemonName' style={inputStyle} placeholder="Enter Pokemon's Name" onChange={pokemonNameHandler}/>
+                    <Button variant="primary" type="submit" size='md'>
+                      Submit
+                    </Button>
+                  </>
+                }
+              </Form.Group>
+            </Form>
+          </Col>
+        </Row>
       </Container>
     </>
   );
